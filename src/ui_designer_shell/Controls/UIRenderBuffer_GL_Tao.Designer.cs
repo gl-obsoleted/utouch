6a3a7ac36879846fa8b92b4a29fa21025446a3c1
gulu <gulu@kingsoft.com>
2014-07-31 10:05:36 +0800

    Gwen 的单元测试现已能跑起来

--- src/ui_designer_shell/Controls/UIRenderBuffer_GL_Tao.Designer.cs
+++ src/ui_designer_shell/Controls/UIRenderBuffer_GL_Tao.Designer.cs
@@ -13,6 +13,8 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+            DisposeUnmanaged();
+
             if (disposing && (components != null))
             {
                 components.Dispose();
@@ -28,10 +30,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.glControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.AccumBits = ((byte)(0));
+            this.glControl.AutoCheckErrors = false;
+            this.glControl.AutoFinish = false;
+            this.glControl.AutoMakeCurrent = true;
+            this.glControl.AutoSwapBuffers = true;
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.ColorBits = ((byte)(32));
+            this.glControl.DepthBits = ((byte)(16));
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(150, 150);
+            this.glControl.StencilBits = ((byte)(0));
+            this.glControl.TabIndex = 0;
+            this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+            this.glControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl_KeyDown);
+            this.glControl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.glControl_KeyPress);
+            this.glControl.KeyUp += new System.Windows.Forms.KeyEventHandler(this.glControl_KeyUp);
+            this.glControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseDown);
+            this.glControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseMove);
+            this.glControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseUp);
+            // 
+            // UIRenderBuffer_GL_Tao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.glControl);
+            this.Name = "UIRenderBuffer_GL_Tao";
+            this.Load += new System.EventHandler(this.UIRenderBuffer_GL_Tao_Load);
+            this.Resize += new System.EventHandler(this.UIRenderBuffer_GL_Tao_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl glControl;
     }
 }
--- src/ui_designer_shell/Controls/UIRenderBuffer_GL_Tao.cs
+++ src/ui_designer_shell/Controls/UIRenderBuffer_GL_Tao.cs
@@ -1,20 +1,233 @@
-﻿using System;
+﻿using Gwen;
+using Gwen.Control;
+using System;
+using System;
+using System.Collections.Generic;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing;
+using System.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms;
+using Tao.OpenGl;
 
 namespace ui_designer_shell.Controls
+//namespace Gwen.Sample.Tao
 {
     public partial class UIRenderBuffer_GL_Tao : UserControl
+    //public partial class GlForm : Form
     {
+        private Canvas canvas;
+        private Gwen.Renderer.Tao renderer;
+        private Gwen.Skin.Base skin;
+        private Gwen.UnitTest.UnitTest test;
+
+        const int fps_frames = 50;
+        private readonly List<long> ftime;
+        private readonly Stopwatch stopwatch;
+        private long lastTime;
+
         public UIRenderBuffer_GL_Tao()
         {
+            ftime = new List<long>();
+            stopwatch = new Stopwatch();
+
             InitializeComponent();
+
+            Width = 1024;
+            Height = 768;
+        }
+
+        private void UIRenderBuffer_GL_Tao_Load(object sender, System.EventArgs e)
+        {
+            glControl.InitializeContexts();
+            Gl.glClearColor(1f, 0f, 0f, 1f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(0, glControl.Width, glControl.Height, 0, -1, 1);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glViewport(0, 0, glControl.Width, glControl.Height);
+
+            renderer = new Gwen.Renderer.Tao();
+            skin = new Gwen.Skin.TexturedBase(renderer, "DefaultSkin.png");
+            canvas = new Canvas(skin);
+            canvas.SetSize(glControl.Width, glControl.Height);
+            canvas.ShouldDrawBackground = true;
+            canvas.BackgroundColor = Color.FromArgb(255, 150, 170, 170);
+            canvas.KeyboardInputEnabled = true;
+            canvas.MouseInputEnabled = true;
+
+            test = new Gwen.UnitTest.UnitTest(canvas);
+
+            stopwatch.Restart();
+            lastTime = 0;
+        }
+
+        private void UIRenderBuffer_GL_Tao_Resize(object sender, System.EventArgs e)
+        {
+            if (canvas != null)
+                canvas.SetSize(glControl.Width, glControl.Height);
+        }
+
+        private void glControl_Paint(object sender, PaintEventArgs e)
+        {
+            if (ftime.Count == fps_frames)
+                ftime.RemoveAt(0);
+
+            ftime.Add(stopwatch.ElapsedMilliseconds - lastTime);
+            lastTime = stopwatch.ElapsedMilliseconds;
+
+            if (stopwatch.ElapsedMilliseconds > 1000)
+            {
+                test.Note = String.Format("String Cache size: {0}", renderer.TextCacheSize);
+                test.Fps = 1000f * ftime.Count / ftime.Sum();
+                stopwatch.Restart();
+
+                if (renderer.TextCacheSize > 1000) // each cached string is an allocated texture, flush the cache once in a while in your real project
+                    renderer.FlushTextCache();
+            }
+
+            Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT | Gl.GL_COLOR_BUFFER_BIT);
+            canvas.RenderCanvas();
+        }
+
+        private void glControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            int btn = -1;
+            if (e.Button == MouseButtons.Left)
+            {
+                btn = 0;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                btn = 1;
+            }
+            canvas.Input_MouseButton(btn, true);
+
+            glControl.Invalidate();
+        }
+
+        private void glControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            int btn = -1;
+            if (e.Button == MouseButtons.Left)
+            {
+                btn = 0;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                btn = 1;
+            }
+            canvas.Input_MouseButton(btn, false);
+
+            glControl.Invalidate();
+        }
+
+        int prevX = -1;
+        int prevY = -1;
+        private void glControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            canvas.Input_MouseMoved(e.X, e.Y, e.X - prevX, e.Y - prevY);
+            prevX = e.X;
+            prevY = e.Y;
+            glControl.Invalidate();
+        }
+
+        private void glControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            canvas.Input_Key(ConvertKeysToGwenKey(e.KeyCode), true);
+            glControl.Invalidate();
+        }
+
+        private void glControl_KeyUp(object sender, KeyEventArgs e)
+        {
+            canvas.Input_Key(ConvertKeysToGwenKey(e.KeyCode), false);
+            glControl.Invalidate();
+        }
+
+        private Key ConvertKeysToGwenKey(Keys keys)
+        {
+            switch (keys)
+            {
+                case Keys.Alt:
+                    return Key.Alt;
+
+                case Keys.Back:
+                    return Key.Backspace;
+
+                case Keys.Control:
+                case Keys.LControlKey:
+                case Keys.RControlKey:
+                    return Key.Control;
+
+                case Keys.Delete:
+                    return Key.Delete;
+
+                case Keys.Down:
+                    return Key.Down;
+
+                case Keys.End:
+                    return Key.End;
+
+                case Keys.Escape:
+                    return Key.Escape;
+
+                case Keys.Home:
+                    return Key.Home;
+
+                case Keys.Left:
+                    return Key.Left;
+
+                case Keys.Return:
+                    return Key.Return;
+
+                case Keys.Right:
+                    return Key.Right;
+
+                case Keys.Shift:
+                case Keys.LShiftKey:
+                case Keys.RShiftKey:
+                    return Key.Shift;
+
+                case Keys.Space:
+                    return Key.Space;
+
+                case Keys.Tab:
+                    return Key.Tab;
+
+                case Keys.Up:
+                    return Key.Up;
+
+                default:
+                    return Key.Invalid;
+            }
+        }
+
+        private void glControl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            canvas.Input_Character(e.KeyChar);
+            glControl.Invalidate();
+        }
+
+        private void DisposeUnmanaged()
+        {
+            //TODO[GL]: 这个对象的 Dispose 内会抛空引用异常，待查
+            try
+            {
+                canvas.Dispose();
+            }
+            catch (NullReferenceException)
+            {
+            }
+
+            skin.Dispose();
+            renderer.Dispose();
         }
     }
 }
