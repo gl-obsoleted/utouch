65e6be031953cc63ee6630b10e43756bf820e178
gulu <gulu@kingsoft.com>
2014-07-31 12:02:18 +0800

    添加 ui_lib 工程

---
+++ src/ui_lib/Elements/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ui_lib.Elements
+{
+    public class Image : Node
+    {
+        public Image() : base()
+        {
+
+        }
+
+        string ResPath { get; set; }
+    }
+}
---
+++ src/ui_lib/Elements/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ui_lib.Elements
+{
+    public class Node
+    {
+        /// <summary>
+        /// 常用属性
+        /// </summary>
+        public Vec2 Position { get; set; }
+        public Size Size { get; set; }
+        public bool Visible { get; set; }
+        public Node Parent { get { return m_parent; } }
+
+        /// <summary>
+        /// 构造一个默认有效的对象（无父节点）
+        /// </summary>
+        public Node()
+        {
+            Visible = true;
+
+            Position = new Vec2(0, 0);
+            Size = new Size(100, 100);
+        }
+
+        /// <summary>
+        /// 我们显式地 SetParent 而不是 set 属性 Parent，语义上更明确.
+        /// </summary>
+        /// <param name="parent"></param>
+        public void SetParent(Node parent)
+        {
+            m_parent = parent;
+        }
+
+        private Node m_parent;
+    }
+}
