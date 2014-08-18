e0f3df2c6c3232ee5fa52c1919e53984943e3397
gulu <gulu@kingsoft.com>
2014-08-18 17:55:51 +0800

    给控件面板添加默认的图标

--- src/ui_designer_shell/WinFormsUI/Controls/UIControlList.cs
+++ src/ui_designer_shell/WinFormsUI/Controls/UIControlList.cs
@@ -15,11 +15,25 @@ namespace ui_designer_shell.WinFormsUI.Controls
         public UIControlList()
         {
             InitializeComponent();
+
+            m_icons.Images.Add(Image.FromFile("media/appbar.folder.png"));
+            m_icons.ImageSize = new Size(48, 48);
+
+            m_controlList.View = View.LargeIcon;
+            m_controlList.LargeImageList = m_icons;
+            m_controlList.SmallImageList = m_icons;
+
+            foreach (ListViewItem it in m_controlList.Items)
+            {
+                it.ImageIndex = 0;
+            }
         }
 
         private void m_controlList_ItemDrag(object sender, ItemDragEventArgs e)
         {
             m_controlList.DoDragDrop(((ListViewItem)e.Item).Text, DragDropEffects.Copy);
         }
+
+        ImageList m_icons = new ImageList();
     }
 }
